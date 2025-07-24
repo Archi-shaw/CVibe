3f79b5dc389b92c8dd9e1443b33bc3d79bc396f0
Archi Shaw <archishaw2004@gmail.com>
2025-07-25 03:20:18 +0530

    changes

--- frontened/vite-project/src/pages/ResumeUpdate/ResumeEdit.jsx
+++ frontened/vite-project/src/pages/ResumeUpdate/ResumeEdit.jsx
@@ -1,10 +1,110 @@
-import React from 'react'
+import React, { useState ,useParams, useRef} from 'react'
+import {useNavigate} from 'react-router-dom';
+import {LuCircleAlert , LuArrowLeft, LuDownload, LuPalette, LuSave, LuTrash2} from 'react-icons/lu';
+import toast from 'react-hot-toast'; 
+import DashboardLayout from '../../components/layouts/DashboardLayout'
+import TitleInput from '../../components/Inputs/TitleInput';
 
 const ResumeEdit = () => {
+  const { resumeId } = useParams();
+  const navigate = useNavigate();
+
+  const resumeRef = useRef(null);
+  const resumeDownloadRef = useRef(null);
+
+  const[baseWidth, setBaseWidth] = useState(800);
+  const[openThemeSelector , setOpenThemeSelector] = useState(false);
+  const[openPreviewModel, setOpenPreviewModel] = useState(false);
+
+  const[currentPage, setCurrentPage] = useState("profile-info");
+  const[progres,setProgress] = useState(0);
+  const[resumeData, setResumeData] = useState({
+    title:"",
+    thumbnailLinks:"",
+    template:{
+        theme:"",
+        colorPaletes: "",
+    },
+    profileinfo:{
+        profileImg: null,
+        profilePreviewUrl: "",
+        name: "",
+        designation:"",
+        summary:"",
+    },
+    contactinfo: {
+        email:"",
+        phone:"",
+        location:"",
+        linkedin: "",
+        github: "",
+        website: "",
+    },
+    workexperience:[
+        {
+            company: "",
+            role: "",
+            startDate: "",
+            endDate: "",
+            description: "",
+        }
+    ],
+    education: [
+        {
+            degree: "",
+            instituition: "",
+            startDate: "",
+            endDate: "",
+        }
+    ],
+    skills:[
+        {
+            name:"",
+            progress: 0,
+        },
+    ],
+    projects:[
+        {
+            title: "",
+            description: "",
+            github:"",
+            livedemo:"",
+        },
+    ],
+    certifications: [
+        {
+            title: "",
+            issuer: "",
+            year: 0,
+        },
+    ],
+    languages: [
+        {
+            name: "",
+            progress: 0,
+        },
+    ],
+    interest: [""],
+  });
+
+  const [errormsg,setErrormsg] =useState("");
+  const[isLoading,setIsLoading] = useState(false);
   return (
-    <div>
-      EditResume
+  <DashboardLayout>
+   <div className='container mx-auto'>
+    <div className='flex items-center justify-between gap-5 bg-white rounded-lg border border-purple-100 py-3 px-4 mb-4'>
+      <TitleInput 
+       title={resumeData.title}
+       setTitle={(value) =>
+        setResumeData((prevState) => ({
+          ...prevState,
+          title: value,
+        }))
+       }
+      />
     </div>
+   </div>
+  </DashboardLayout>
   )
 }
 
